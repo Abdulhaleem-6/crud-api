c5b78f6f458d9dda4fed266750afb58b1e3096d8
Abdulhaleem-6 <Abd6prif@gmail.com>
2023-09-15 03:11:01 +0100

    updating the PersonDetail view

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,6 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.PersonListCreateAPI.as_view()),
     path('<int:pk>/', views.PersonDetail.as_view()),
-    path('<int:pk>/', views.PersonUpdateAPIView.as_view()),
-    path('<int:pk>/', views.PersonDeleteAPIView.as_view())
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -23,9 +23,10 @@ class PersonListCreateAPI(generics.ListCreateAPIView):
         return Response(response_data)
         
 
-class PersonDetail(generics.RetrieveAPIView):
+class PersonDetail(generics.RetrieveUpdateAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
+    lookup_field = 'pk'
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -37,13 +38,8 @@ class PersonDetail(generics.RetrieveAPIView):
         } 
 
         return Response(response_data)
-
-class PersonUpdateAPIView(generics.UpdateAPIView):
-    queryset = Person.objects.all()
-    serializer_class = PersonSerializer
-    lookup_field = 'pk'
-
-    def update(self, request, *args, **kwargs):
+    
+    def put(self, request, *args, **kwargs):
         try:
             instance = self.get_object()
             serializer = self.get_serializer(instance, data=request.data)
@@ -67,15 +63,8 @@ class PersonUpdateAPIView(generics.UpdateAPIView):
                 'error': str(e)
             }
             return Response(response_data)
-
-
-    
-                    
-class PersonDeleteAPIView(generics.DestroyAPIView):
-    queryset = Person.objects.all()
-    serializer_class = PersonSerializer
-
-    def destroy(self, request, *args, **kwargs):
+        
+    def delete(self, request, *args, **kwargs):
         instance = self.get_object()
         instance.delete()
 
@@ -85,4 +74,55 @@ class PersonDeleteAPIView(generics.DestroyAPIView):
         } 
 
         return Response(response_data)
+    
+
+
+
+# class PersonUpdateAPIView(generics.UpdateAPIView):
+#     queryset = Person.objects.all()
+#     serializer_class = PersonSerializer
+#     lookup_field = 'pk'
+
+#     def update(self, request, *args, **kwargs):
+#         try:
+#             instance = self.get_object()
+#             serializer = self.get_serializer(instance, data=request.data)
+#             serializer.is_valid(raise_exception= True)
+#             serializer.save()
+
+#             response_data = {
+#                 'success': True,
+#                 'message': 'Person updated successfully',
+#                 'data': {
+#                     'id': instance.pk,
+#                     'name': instance.name
+#                 }
+#             }
+#             return Response(response_data)
+
+#         except Exception as e:
+#             response_data = {
+#                 'success' : False,
+#                 'message' : 'Failed to update person',
+#                 'error': str(e)
+#             }
+#             return Response(response_data)
+
+
+    
+                    
+# class PersonDeleteAPIView(generics.DestroyAPIView):
+#     queryset = Person.objects.all()
+#     serializer_class = PersonSerializer
+
+#     def destroy(self, request, *args, **kwargs):
+#         instance = self.get_object()
+#         instance.delete()
+
+#         response_data = {
+#             'success' : True,
+#             'message' : 'Person deleted successfully'
+#         } 
+
+#         return Response(response_data)
     
