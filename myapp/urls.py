a78f45cc30fc0e2b6fa33144f45ec38973d3e0c9
Abdulhaleem-6 <Abd6prif@gmail.com>
2023-09-15 01:33:07 +0100

    trying to sort static files

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.PersonListCreateAPI.as_view()),
     path('<int:pk>/', views.PersonDetail.as_view()),
-    path('<int:pk>/update/', views.PersonUpdateAPIView.as_view()),
-    path('<int:pk>/delete/', views.PersonDeleteAPIView.as_view())
+    path('<int:pk>/', views.PersonUpdateAPIView.as_view()),
+    path('<int:pk>/', views.PersonDeleteAPIView.as_view())
 ]
